8a88b63e1d44d7bbb800561ad7c62c6ef0655c70
Chuks Grinage <chuksgrinage.dev@gmail.com>
2021-07-12 22:54:37 -0500

    first attempt at implementing social auth0

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ariadne.contrib.django',
+    'social_django',
     'posts',
     'users'
 ]
