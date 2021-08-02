b00aeffa69c4d52535afec41d2c7315195601f72
Chuks Grinage <chuksgrinage.dev@gmail.com>
2021-08-01 22:36:50 -0500

    make changes to db

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -13,6 +15,7 @@ class User(AbstractUser, models.Model):
 
 
 class Student(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     teacher = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     dob = models.DateField(null=True, blank=True)
     grade = models.CharField(max_length=3)
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -46,6 +47,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "core.urls"
+
+AUTHENTICATION_BACKENDS = [
+    "ariadne_jwt.backends.JSONWebTokenBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
+
+
+GRAPHQL_JWT = {"JWT_VERIFY_EXPIRATION": True, "JWT_EXPIRATION_DELTA": timedelta(days=30)}
 
 TEMPLATES = [
     {
@@ -120,13 +129,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
-AUTHENTICATION_BACKENDS = [
-    "ariadne_jwt.backends.JSONWebTokenBackend",
-    "django.contrib.auth.backends.ModelBackend",
-]
-
 CORS_ALLOW_CREDENTIALS = True
 
-GRAPHQL_JWT = {"JWT_VERIFY_EXPIRATION": True, "JWT_EXPIRATION_DELTA": timedelta(days=30)}
 
 AUTH_USER_MODEL = "account.User"
