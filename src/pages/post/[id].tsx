1f2e6b1028c57a92bca3c80aaa1bd9ec2fc6db15
Chuks Grinage <chuksgrinage.dev@gmail.com>
2021-10-10 18:49:13 -0500

    update post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,13 +1,17 @@
 import { Button } from '@chakra-ui/button'
-import { Input } from '@chakra-ui/input'
-import { VStack, Text } from '@chakra-ui/layout'
+import { FormControl, FormErrorMessage } from '@chakra-ui/form-control'
+import { VStack, Text, Heading, Grid, GridItem, Box, Link } from '@chakra-ui/layout'
+import { Avatar } from '@chakra-ui/react'
+import { Textarea } from '@chakra-ui/textarea'
 import { useAddCommentMutation, useGetPostByIdQuery } from 'generated'
 import { useRouter } from 'next/router'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import { useAuth } from 'shared/components'
 
+import NexLink from 'next/link'
 const Index = () => {
-  const { query: { id } = {}, back, isReady } = useRouter()
+  const { query: { id } = {}, isReady } = useRouter()
   const { user } = useAuth()
   const postId = id as string
   const {
@@ -16,43 +20,82 @@ const Index = () => {
     isError,
   } = useGetPostByIdQuery({ postId }, { enabled: isReady })
 
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm()
+
   const { mutate: addComment } = useAddCommentMutation()
-  const [comment, setComment] = React.useState('')
 
   if (!isReady) return null
+
+  const onSubmit = ({ body }) => {
+    addComment({ postId, body })
+    reset()
+  }
+
   return (
-    <VStack>
-      <Button onClick={() => back()}>Go Back</Button>
+    <VStack align='start'>
       {isLoading ? (
         <Text>...loading</Text>
       ) : isError ? (
         <Text>Something went wrong</Text>
       ) : (
         <>
-          <Text>{post.title}</Text>
-          <Text>{post.body}</Text>
-          <Text>Here are the comments</Text>
+          <Heading as='h1'>{post.title}</Heading>
+          <Text color='grey'>{post.commentCount} Replies</Text>
           {isLoading || isError ? (
             <div>loading comments</div>
           ) : (
-            post.allComments?.map((comment) => (
-              <Text color={user?.id === comment.user.id ? 'orangered' : null} key={comment.id}>
-                {comment.user.email}: {comment.body}
-              </Text>
-            ))
-          )}
-          {user && (
-            <>
-              <Input value={comment} onChange={(e) => setComment(e.target.value)} />
-              <Button
-                onClick={() => {
-                  addComment({ body: comment, postId })
-                  setComment('')
-                }}
-              >
-                Submit
-              </Button>
-            </>
+            <Grid p='10' templateColumns='repeat(5, 1fr)' gap={10} w='full'>
+              {post.allComments?.map((comment) => (
+                <>
+                  <GridItem key={comment.id} colSpan={1}>
+                    <VStack justify='center'>
+                      <Avatar />
+                      <Text>{comment.user.firstName}</Text>
+                    </VStack>
+                  </GridItem>
+                  <GridItem colSpan={4}>
+                    <VStack align='start'>
+                      <Box>
+                        <NexLink href='/user/[id]' as={`/user/${comment.user.id}`}>
+                          <Link
+                            textTransform='capitalize'
+                            as='em'
+                            color='teal'
+                          >{`${comment.user.firstName} ${comment.user.lastName}`}</Link>
+                        </NexLink>
+                        <Text as='i' color='grey'>
+                          , Teacher in Dallas, TX
+                        </Text>
+                      </Box>
+                      <Text>{comment.body}</Text>
+                    </VStack>
+                  </GridItem>
+                </>
+              ))}
+              {user && (
+                <GridItem as='form' colStart={2} colSpan={4} onSubmit={handleSubmit(onSubmit)}>
+                  <FormControl h='fit-content' isInvalid={errors.content}>
+                    <Textarea
+                      id='body'
+                      h='xs'
+                      placeholder='Reply to post...'
+                      {...register('body', {
+                        required: 'body is required',
+                      })}
+                    />
+                    <FormErrorMessage>{errors?.content?.message}</FormErrorMessage>
+                  </FormControl>
+                  <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
+                    Post Reply
+                  </Button>
+                </GridItem>
+              )}
+            </Grid>
           )}
         </>
       )}
